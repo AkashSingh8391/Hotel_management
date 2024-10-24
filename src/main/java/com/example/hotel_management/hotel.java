e2987d4d55db327438c010ce149e40b8df3b6f91
Akash Singh <akashgurjar6266@gmail.com>
2024-10-24 19:19:38 +0530

    Hotel management using jdbc

--- src/main/java/com/example/hotel_management/hotel.java
+++ src/main/java/com/example/hotel_management/hotel.java
@@ -1,6 +1,29 @@
 package com.example.hotel_management;
 
-public class hotel {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class hotel {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/hotel_management";
+        String user = "root";
+        String password = "Shitlamaa@8989";
+       try{
+           Class.forName("com.mysql.jdbc.Driver");
+
+       }catch(ClassNotFoundException e){
+           System.out.println(e.getMessage());
+       }
+       try{
+           Connection connection= DriverManager.getConnection(url,user,password);
+           System.out.println("Connection successfully...");
+           System.out.println(connection);
+
+       }catch(SQLException e){
+           System.out.println(e.getMessage());
+       }
+
+    }
 
     }
